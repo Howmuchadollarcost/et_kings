590dde805bb97a34fb654ba03a1088d627e64fee
Howmuchadollarcost <muluyonas@yahoo.com>
2020-01-13 17:02:02 +0100

    Store data in translations file

--- src/App.js
+++ src/App.js
@@ -6,94 +6,10 @@ import Title from "./container/Title";
 import Cards from "./container/Cards";
 import Footer from './container/Footer';
 
-const translations = {
-  title: {
-    en_US: "Ethiopian Kings",
-    am_ET: "የኢትዮጵያ ነገስታት"
-  },
-  king0: {
-    en_US: [
-      {
-        name: "Tewodros II ",
-        bornName: "Kassa Hailegiorgis",
-        reign: "1855-1868",
-        aged: "50",
-        house: "House of Solomon",
-        horse: "Abba tatek",
-        religion: "Orthodox",
-        image:
-          "https://www.nazret.com/wp-content/uploads/2019/03/px800-tewodros-ii.jpg"
-      }
-    ],
-    am_ET: [
-      {
-        name: "ዳግማዊ ዓፄ ቴዎድሮስ",
-        bornName: "ካሣ ኃይለ ጊዮርጊስ",
-        reign: "1855-1868",
-        aged: "50",
-        house: "ሰሎሞን",
-        horse: "አባ ታጠቅ",
-        religion: "ኦርቶዶክስ ተዋሕዶ ክርስትና",
-        image:
-          "https://www.nazret.com/wp-content/uploads/2019/03/px800-tewodros-ii.jpg"
-      }
-    ]
-  },
-  king1: {
-    en_US: [
-      {
-        name: "Yohannes IV",
-        bornName: "Kassa Mercha",
-        reign: "1871-1889",
-        aged: "51",
-        house: "House of Solomon",
-        horse: "Abba Bezbez",
-        religion: "Orthodox",
-        image: "https://farm3.static.flickr.com/2091/2058092676_23985f2f50.jpg"
-      }
-    ],
-    am_ET: [
-      {
-        name: "ዓፄ ዮሐንስ ፬ኛ",
-        bornName: " ካሳ ምርጫ",
-        reign: "1871-1889",
-        aged: "51",
-        house: "ሰሎሞን",
-        horse: "አባ በዝብዝ",
-        religion: "ኦርቶዶክስ ተዋሕዶ",
-        image:"https://farm3.static.flickr.com/2091/2058092676_23985f2f50.jpg"
-      }
-    ]
-  },
-  king2: {
-    en_US: [
-      {
-        name: "Menelik II",
-        bornName: "Sahle Maryam",
-        reign: "1889-1913",
-        aged: "69",
-        house: "House of Solomon",
-        horse: "Abba dagnew",
-        religion: "Orthodox",
-        image:
-          "https://www.biography.com/.image/t_share/MTE5NDg0MDU1MDY1ODIyNzM1/menelik-ii-9405715-1-420.jpg"
-      }
-    ],
-    am_ET: [
-      {
-        name: "ዳግማዊ ዓፄ ምኒልክ",
-        bornName: "ሳህለ ማርያም",
-        reign: "1889-1913",
-        aged: "69",
-        house: "ሰሎሞን",
-        horse: "አባ ዳኘው",
-        religion: "ኦርቶዶክስ ክርስትና",
-        image:
-          "https://www.biography.com/.image/t_share/MTE5NDg0MDU1MDY1ODIyNzM1/menelik-ii-9405715-1-420.jpg"
-      }
-    ]
-  }
-};
+import translationData from './translations';
+
+const translations = JSON.parse(translationData);
+
 
 const Container = styled.div`
   display: flex;
